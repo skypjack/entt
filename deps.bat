87a1062bdeea8fa9273fda7c64555820532e222f
Michele Caini <michele.caini@gmail.com>
2017-05-22 22:25:37 +0200

    appveyor stuff

--- deps.bat
+++ deps.bat
@@ -2,7 +2,6 @@
 set SRC_DIR=%~dp0
 
 if not defined ARCH set ARCH=x86
-if defined platform if "%platform%"=="x64" set ARCH=x64
 
 @rem init/update submodules
 cd %SRC_DIR%
@@ -10,11 +9,7 @@ git submodule update --init
 
 @rem compile dependencies
 
-if "%msbuild_platform%"=="x64" (
-    set CMAKE_GENERATOR_NAME=Visual Studio 14 2015 Win64
-) else (
-    set CMAKE_GENERATOR_NAME=Visual Studio 14 2015
-)
+set CMAKE_GENERATOR_NAME=Visual Studio 14 2015
 
 set GTEST_BUILD_DIR=%SRC_DIR%deps\googletest\build
 if exist %GTEST_BUILD_DIR%\NUL rd /s /q %GTEST_BUILD_DIR%
