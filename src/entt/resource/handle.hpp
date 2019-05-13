d56cab70b0223f6855fbce57bf9df0051d03d878
The5-1 <agaku@hotmail.de>
2019-05-13 22:20:23 +0200

    fixed indentation

--- src/entt/resource/handle.hpp
+++ src/entt/resource/handle.hpp
@@ -11,147 +11,147 @@
 namespace entt {
 
 
-	/**
-	 * @brief Shared resource handle.
-	 *
-	 * A shared resource handle is a small class that wraps a resource and keeps it
-	 * alive even if it's deleted from the cache. It can be either copied or
-	 * moved. A handle shares a reference to the same resource with all the other
-	 * handles constructed for the same identifier.<br/>
-	 * As a rule of thumb, resources should never be copied nor moved. Handles are
-	 * the way to go to keep references to them.
-	 *
-	 * @tparam Resource Type of resource managed by a handle.
-	 */
-	template<typename Resource>
-	class resource_handle {
-		/*! @brief Resource handles are friends of their caches. */
-		friend class resource_cache<Resource>;
+/**
+ * @brief Shared resource handle.
+ *
+ * A shared resource handle is a small class that wraps a resource and keeps it
+ * alive even if it's deleted from the cache. It can be either copied or
+ * moved. A handle shares a reference to the same resource with all the other
+ * handles constructed for the same identifier.<br/>
+ * As a rule of thumb, resources should never be copied nor moved. Handles are
+ * the way to go to keep references to them.
+ *
+ * @tparam Resource Type of resource managed by a handle.
+ */
+template<typename Resource>
+class resource_handle {
+    /*! @brief Resource handles are friends of their caches. */
+    friend class resource_cache<Resource>;
 
-		resource_handle(std::shared_ptr<Resource> res) ENTT_NOEXCEPT
-			: resource{std::move(res)}
-		{}
+    resource_handle(std::shared_ptr<Resource> res) ENTT_NOEXCEPT
+        : resource{std::move(res)}
+    {}
 
-	public:
-		/*! @brief Default constructor. */
-		resource_handle() ENTT_NOEXCEPT = default;
+public:
+    /*! @brief Default constructor. */
+    resource_handle() ENTT_NOEXCEPT = default;
 
-		/**
-		 * @brief Gets a reference to the managed resource.
-		 *
-		 * @warning
-		 * The behavior is undefined if the handle doesn't contain a resource.<br/>
-		 * An assertion will abort the execution at runtime in debug mode if the
-		 * handle is empty.
-		 *
-		 * @return A reference to the managed resource.
-		 */
-		const Resource& get() const ENTT_NOEXCEPT {
-			ENTT_ASSERT(static_cast<bool>(resource));
-			return *resource;
-		}
+    /**
+     * @brief Gets a reference to the managed resource.
+     *
+     * @warning
+     * The behavior is undefined if the handle doesn't contain a resource.<br/>
+     * An assertion will abort the execution at runtime in debug mode if the
+     * handle is empty.
+     *
+     * @return A reference to the managed resource.
+     */
+    const Resource& get() const ENTT_NOEXCEPT {
+        ENTT_ASSERT(static_cast<bool>(resource));
+        return *resource;
+    }
 
-		/**
-		 * @brief Gets a mutable reference to the managed resource.
-		 *
-		 * @warning
-		 * The behavior is undefined if the handle doesn't contain a resource.<br/>
-		 * An assertion will abort the execution at runtime in debug mode if the
-		 * handle is empty.
-		 *
-		 * @return A mutable reference to the managed resource.
-		 */
-		Resource& get() ENTT_NOEXCEPT {
-			ENTT_ASSERT(static_cast<bool>(resource));
-			return const_cast<Resource&>(*resource);
-		}
+    /**
+     * @brief Gets a mutable reference to the managed resource.
+     *
+     * @warning
+     * The behavior is undefined if the handle doesn't contain a resource.<br/>
+     * An assertion will abort the execution at runtime in debug mode if the
+     * handle is empty.
+     *
+     * @return A mutable reference to the managed resource.
+     */
+    Resource& get() ENTT_NOEXCEPT {
+        ENTT_ASSERT(static_cast<bool>(resource));
+        return const_cast<Resource&>(*resource);
+    }
 
-		/**
-		 * @brief Casts a handle and gets a reference to the managed resource.
-		 *
-		 * @warning
-		 * The behavior is undefined if the handle doesn't contain a resource.<br/>
-		 * An assertion will abort the execution at runtime in debug mode if the
-		 * handle is empty.
-		 */
-		inline operator const Resource& () const ENTT_NOEXCEPT { return get(); }
+    /**
+     * @brief Casts a handle and gets a reference to the managed resource.
+     *
+     * @warning
+     * The behavior is undefined if the handle doesn't contain a resource.<br/>
+     * An assertion will abort the execution at runtime in debug mode if the
+     * handle is empty.
+     */
+    inline operator const Resource& () const ENTT_NOEXCEPT { return get(); }
 
-		/**
-		 * @brief Casts a handle and gets a mutable reference to the managed resource.
-		 *
-		 * @warning
-		 * The behavior is undefined if the handle doesn't contain a resource.<br/>
-		 * An assertion will abort the execution at runtime in debug mode if the
-		 * handle is empty.
-		 */
-		inline operator Resource& () ENTT_NOEXCEPT { return get(); }
+    /**
+     * @brief Casts a handle and gets a mutable reference to the managed resource.
+     *
+     * @warning
+     * The behavior is undefined if the handle doesn't contain a resource.<br/>
+     * An assertion will abort the execution at runtime in debug mode if the
+     * handle is empty.
+     */
+    inline operator Resource& () ENTT_NOEXCEPT { return get(); }
 
-		/**
-		 * @brief Dereferences a handle to obtain the managed resource.
-		 *
-		 * @warning
-		 * The behavior is undefined if the handle doesn't contain a resource.<br/>
-		 * An assertion will abort the execution at runtime in debug mode if the
-		 * handle is empty.
-		 *
-		 * @return A reference to the managed resource.
-		 */
-		inline const Resource& operator *() const ENTT_NOEXCEPT { return get(); }
+    /**
+     * @brief Dereferences a handle to obtain the managed resource.
+     *
+     * @warning
+     * The behavior is undefined if the handle doesn't contain a resource.<br/>
+     * An assertion will abort the execution at runtime in debug mode if the
+     * handle is empty.
+     *
+     * @return A reference to the managed resource.
+     */
+    inline const Resource& operator *() const ENTT_NOEXCEPT { return get(); }
 
 
-		/**
-		 * @brief Dereferences a handle to obtain the managed resource.
-		 *
-		 * @warning
-		 * The behavior is undefined if the handle doesn't contain a resource.<br/>
-		 * An assertion will abort the execution at runtime in debug mode if the
-		 * handle is empty.
-		 *
-		 * @return A mutable reference to the managed resource.
-		 */
-		inline Resource& operator *() ENTT_NOEXCEPT { return get(); }
+    /**
+     * @brief Dereferences a handle to obtain the managed resource.
+     *
+     * @warning
+     * The behavior is undefined if the handle doesn't contain a resource.<br/>
+     * An assertion will abort the execution at runtime in debug mode if the
+     * handle is empty.
+     *
+     * @return A mutable reference to the managed resource.
+     */
+    inline Resource& operator *() ENTT_NOEXCEPT { return get(); }
 
-		/**
-		 * @brief Gets a pointer to the managed resource from a handle.
-		 *
-		 * @warning
-		 * The behavior is undefined if the handle doesn't contain a resource.<br/>
-		 * An assertion will abort the execution at runtime in debug mode if the
-		 * handle is empty.
-		 *
-		 * @return A pointer to the managed resource or `nullptr` if the handle
-		 * contains no resource at all.
-		 */
-		inline const Resource* operator->() const ENTT_NOEXCEPT {
-			ENTT_ASSERT(static_cast<bool>(resource));
-			return resource.get();
-		}
+    /**
+     * @brief Gets a pointer to the managed resource from a handle.
+     *
+     * @warning
+     * The behavior is undefined if the handle doesn't contain a resource.<br/>
+     * An assertion will abort the execution at runtime in debug mode if the
+     * handle is empty.
+     *
+     * @return A pointer to the managed resource or `nullptr` if the handle
+     * contains no resource at all.
+     */
+    inline const Resource* operator->() const ENTT_NOEXCEPT {
+        ENTT_ASSERT(static_cast<bool>(resource));
+        return resource.get();
+    }
 
-		/**
-		 * @brief Gets a mutable pointer to the managed resource from a handle.
-		 *
-		 * @warning
-		 * The behavior is undefined if the handle doesn't contain a resource.<br/>
-		 * An assertion will abort the execution at runtime in debug mode if the
-		 * handle is empty.
-		 *
-		 * @return A mutable pointer to the managed resource or `nullptr` if the handle
-		 * contains no resource at all.
-		 */
-		inline Resource* operator->() ENTT_NOEXCEPT {
-			ENTT_ASSERT(static_cast<bool>(resource));
-			return resource.get();
-		}
+    /**
+     * @brief Gets a mutable pointer to the managed resource from a handle.
+     *
+     * @warning
+     * The behavior is undefined if the handle doesn't contain a resource.<br/>
+     * An assertion will abort the execution at runtime in debug mode if the
+     * handle is empty.
+     *
+     * @return A mutable pointer to the managed resource or `nullptr` if the handle
+     * contains no resource at all.
+     */
+    inline Resource* operator->() ENTT_NOEXCEPT {
+        ENTT_ASSERT(static_cast<bool>(resource));
+        return resource.get();
+    }
 
-		/**
-		 * @brief Returns true if a handle contains a resource, false otherwise.
-		 * @return True if the handle contains a resource, false otherwise.
-		 */
-		explicit operator bool() const { return static_cast<bool>(resource); }
+    /**
+     * @brief Returns true if a handle contains a resource, false otherwise.
+     * @return True if the handle contains a resource, false otherwise.
+     */
+    explicit operator bool() const { return static_cast<bool>(resource); }
 
-	private:
-		std::shared_ptr<Resource> resource;
-	};
+private:
+    std::shared_ptr<Resource> resource;
+};
 
 
 }
