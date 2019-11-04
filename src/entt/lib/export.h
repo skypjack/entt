ef8bb3fbd9993d1b7a622e03cad0b302d4cddb12
Innokentiy Alaytsev <alaitsev@gmail.com>
2019-11-04 23:28:23 +0400

    Removed the unneeded export of type id member variable
    Signed-off-by: Innokentiy Alaytsev <alaitsev@gmail.com>

--- src/entt/lib/export.h
+++ src/entt/lib/export.h
@@ -6,29 +6,6 @@
 #include "../core/family.hpp"
 
 
-/**
-   @brief Exports the type id of the given class in the family with the given
-   tag
-
-   @details For unknown reasons exporting the type id within the family seems to
-   be required for GCC and Clang. MSVC doesn't allow importing member variables
-   so exporting it makes no sense.
-
-   @param family_tag The tag of the family for which to export the type id.
-   @param clazz The type for which to export its type id withic the family.
-   @param annribute The attribute macro that should expand into either export or
-   import clause depending on whether the exported library is build or consumed
-   respectively.
-*/
-#if defined _WIN32 || defined __CYGWIN__ || defined _MSC_VER
-#define ENTT_EXPORT_FAMILY_TYPE_ID(family_tag, clazz, attribute) static_assert(true)
-#else
-#define ENTT_EXPORT_FAMILY_TYPE_ID(family_tag, clazz, attribute)			\
-	static_assert(true)
-//	template attribute const ENTT_ID_TYPE family<family_tag>::type<clazz>
-#endif
-
-
 #define ENTT_FAMILY_IDENTIFIER_GENERATOR_EXPORT(family_tag, attribute)	\
 	namespace entt {\
         template attribute ENTT_ID_TYPE family<family_tag>::generate_identifier();\
@@ -39,7 +16,6 @@
 #define ENTT_FAMILY_TYPE_IDENTIFIER_EXPORT(family_tag, clazz, attribute) \
 	namespace entt {\
         template attribute ENTT_ID_TYPE family<family_tag>::generate_type_id<clazz>();\
-		ENTT_EXPORT_FAMILY_TYPE_ID(family_tag, clazz, attribute); \
     }\
     static_assert(true)
 
