86a392991a998333ce4e9160f3fb31c757c8f035
Michele Caini <michele.caini@gmail.com>
2020-03-28 17:05:23 +0100

    test: type_info::index

--- test/entt/core/type_info.cpp
+++ test/entt/core/type_info.cpp
@@ -2,8 +2,15 @@
 #include <entt/core/hashed_string.hpp>
 #include <entt/core/type_info.hpp>
 
-TEST(TypeId, Functionalities) {
+TEST(TypeInfo, Id) {
     ASSERT_NE(entt::type_info<int>::id(), entt::type_info<const int>::id());
     ASSERT_NE(entt::type_info<int>::id(), entt::type_info<char>::id());
     ASSERT_EQ(entt::type_info<int>::id(), entt::type_info<int>::id());
+}
+
+TEST(TypeInfo, Index) {
+    ASSERT_EQ(entt::type_info<int>::index(), entt::type_info<int>::index());
+    ASSERT_NE(entt::type_info<int>::index(), entt::type_info<char>::index());
+    ASSERT_NE(entt::type_info<int>::index(), entt::type_info<int &&>::index());
+    ASSERT_NE(entt::type_info<int &>::index(), entt::type_info<const int &>::index());
 }
