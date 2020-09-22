c49227d82878de2cd4ae3decf93cfe4d097e994e
Michele Caini <michele.caini@gmail.com>
2020-09-22 08:42:41 +0200

    API reference v3.5.2

---
+++ search/related_5.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['type_5fid_866',['type_id',['../classentt_1_1type__info.html#a26e32f48f686578bc94644c2e64d0f71',1,'entt::type_info']]]
+];
---
+++ search/variables_b.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['version_5fmask_806',['version_mask',['../structentt_1_1entt__traits_3_01std_1_1uint32__t_01_4.html#aad3917f55015f41728684c3727643b31',1,'entt::entt_traits&lt; std::uint32_t &gt;::version_mask()'],['../structentt_1_1entt__traits_3_01std_1_1uint64__t_01_4.html#a71ffda25f3b437989eaea515728afd15',1,'entt::entt_traits&lt; std::uint64_t &gt;::version_mask()']]]
+];
