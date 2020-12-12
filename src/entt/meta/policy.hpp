2d17ef5c7e3df330c236c156fb5158d21675cb2e
Michele Caini <michele.caini@gmail.com>
2020-12-12 14:58:31 +0100

    meta: removed as_ref tag

--- src/entt/meta/policy.hpp
+++ src/entt/meta/policy.hpp
@@ -9,10 +9,6 @@ namespace entt {
 struct as_ref_t {};
 
 
-/*! @brief Disambiguation tag. */
-inline constexpr as_ref_t as_ref;
-
-
 /*! @brief Empty class type used to request the _as-is_ policy. */
 struct as_is_t {};
 
