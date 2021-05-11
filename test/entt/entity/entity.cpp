9af318a767b0204492d132c1c03e25e4c0ba123e
Michele Caini <michele.caini@gmail.com>
2021-05-11 12:05:44 +0200

    test: minor changes

--- test/entt/entity/entity.cpp
+++ test/entt/entity/entity.cpp
@@ -7,17 +7,17 @@
 TEST(Entity, Null) {
     using traits_type = entt::entt_traits<entt::entity>;
 
-    entt::registry registry{};
-    const auto entity = registry.create();
-
-    registry.emplace<int>(entity, 42);
-
     ASSERT_FALSE(entt::entity{} == entt::null);
     ASSERT_TRUE(entt::entity{traits_type::entity_mask} == entt::null);
     ASSERT_TRUE(entt::entity{~typename traits_type::entity_type{}} == entt::null);
 
     ASSERT_TRUE(entt::null == entt::null);
     ASSERT_FALSE(entt::null != entt::null);
+
+    entt::registry registry{};
+    auto entity = registry.create();
+
+    registry.emplace<int>(entity, 42);
 
     ASSERT_FALSE(entity == entt::null);
     ASSERT_FALSE(entt::null == entity);
@@ -26,4 +26,5 @@ TEST(Entity, Null) {
     ASSERT_TRUE(entt::null != entity);
 
     ASSERT_FALSE(registry.valid(entt::null));
+    ASSERT_DEATH((entity = registry.create(entt::null)), "");
 }
