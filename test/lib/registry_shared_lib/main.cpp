e0083abb8162bf0739dec5d1b02a80367a747282
s.kolesnik <7fosGbc9S>
2021-07-08 00:00:29 +0500

    #719 added test for registry as a shared library: comparing ids of components within an executable

---
+++ test/lib/registry_shared_lib/main.cpp
@@ -0,0 +1,30 @@
+﻿
+#ifndef ENTT_API_IMPORT
+#error "ENTT_API_IMPORT must be defined when using entt as a shared library"
+#endif
+
+#include "api.h"
+
+#include <gtest/gtest.h>
+
+struct zero_component {};
+
+/**
+ * This test will fail if entt::internal::type_seq::next() was not linked
+ * (imported/exported from shared registry). Two different versions will be used
+ * by template specializations of entt::type_seq.
+ *
+ * This test will not fail (for a wrong reason) if linker has not imported or
+ * optimized out imported entt symbols (will fail without optimization). Thus
+ * both type ids will use ent::internal::type_seq::next::value generated within
+ * the executable.
+ */
+TEST(shared_registry_lib, compare_ids_within_core) {
+  EXPECT_EQ(entt::type_seq<zero_component>(), 0);
+  ASSERT_EQ(entt::type_seq<test_increment>(), 1);
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
---
+++ test/lib/registry_shared_lib/plugin.cpp
@@ -0,0 +1,33 @@
+﻿
+#include "api.h"
+
+#include <iterator>
+
+extern "C" {
+int get_id(entt::registry &);
+}
+
+/**
+ * If functions are compiled inlined, there might be 2 situations:
+ * - symbols were not exported by the linker from shared registry library
+ * - symbols were not imported to the plugin due to linker optimization
+ *
+ * The context of the test implies that another type has been registered prior
+ * to an exported, hence if an imported symbol is used, the id will be equal to
+ * 1. Otherwise it will be equal to 0, and there will be two identical
+ * identifiers stored in duplicated symbols.
+ *
+ * In case of linker optimization on importing, the entity will not be found
+ * within the registry.
+ *
+ * @param registry
+ * @return
+ */
+int get_id(entt::registry &registry) {
+
+  const auto id = entt::type_seq<test_increment>();
+  auto view = registry.view<test_increment>();
+  if (std::distance(view.begin(), view.end()))
+    ++(registry.get<test_increment>(view[0]).i);
+  return static_cast<int>(id);
+}
