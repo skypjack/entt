f6aaafd60fe4e7b79750dee5c623ccc67aa2183f
Michele Caini <michele.caini@gmail.com>
2022-03-22 12:43:47 +0100

    config: add ENTT_NOEXCEPT_IF (for future uses)

--- src/entt/config/config.h
+++ src/entt/config/config.h
@@ -5,11 +5,13 @@
 
 #if defined(__cpp_exceptions) && !defined(ENTT_NOEXCEPTION)
 #    define ENTT_NOEXCEPT noexcept
+#    define ENTT_NOEXCEPT_IF(expr) noexcept(expr)
 #    define ENTT_THROW throw
 #    define ENTT_TRY try
 #    define ENTT_CATCH catch(...)
 #else
 #    define ENTT_NOEXCEPT
+#    define ENTT_NOEXCEPT_IF(...)
 #    define ENTT_THROW
 #    define ENTT_TRY if(true)
 #    define ENTT_CATCH if(false)
