325a5dd353c6ffccdc8ec4ba713a76fe3472f44d
Michele Caini <michele.caini@gmail.com>
2022-07-29 10:46:15 +0200

    test: utilities to suppress death tests in release

---
+++ test/entt/common/config.h
@@ -0,0 +1,18 @@
+#ifndef ENTT_COMMON_CONFIG_H
+#define ENTT_COMMON_CONFIG_H
+
+namespace test {
+
+#ifdef NDEBUG
+#    define ENTT_DEBUG_TEST(Case, Test) TEST(Case, DISABLED_##Test)
+#    define ENTT_DEBUG_TEST_F(Case, Test) TEST_F(Case, DISABLED_##Test)
+#    define ENTT_DEBUG_TYPED_TEST(Case, Test) TYPED_TEST(Case, DISABLED_##Test)
+#else
+#    define ENTT_DEBUG_TEST(Case, Test) TEST(Case, Test)
+#    define ENTT_DEBUG_TEST_F(Case, Test) TEST_F(Case, Test)
+#    define ENTT_DEBUG_TYPED_TEST(Case, Test) TYPED_TEST(Case, Test)
+#endif
+
+} // namespace test
+
+#endif
