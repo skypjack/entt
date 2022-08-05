d9b1cc0db34823bc57b09cc0cd7575adc6d48eca
Michele Caini <michele.caini@gmail.com>
2022-08-05 12:11:06 +0200

    iwyu: begin/end exports directive for entt.hpp/fwd.hpp (see #777)

--- src/entt/entt.hpp
+++ src/entt/entt.hpp
@@ -1,3 +1,4 @@
+// IWYU pragma: begin_exports
 #include "config/config.h"
 #include "config/macro.h"
 #include "config/version.h"
@@ -60,3 +61,4 @@
 #include "signal/dispatcher.hpp"
 #include "signal/emitter.hpp"
 #include "signal/sigh.hpp"
+// IWYU pragma: end_exports
