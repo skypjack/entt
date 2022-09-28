7a3f59cc5d8258210fd42d4bd89848a002ac4d3d
Michele Caini <michele.caini@gmail.com>
2022-09-28 18:14:04 +0200

    delegate: drop useless scope

--- src/entt/signal/delegate.hpp
+++ src/entt/signal/delegate.hpp
@@ -35,7 +35,7 @@ template<typename Class, typename Type, typename... Other>
 constexpr auto function_pointer(Type Class::*, Other &&...) -> Type (*)();
 
 template<typename... Type>
-using function_pointer_t = decltype(internal::function_pointer(std::declval<Type>()...));
+using function_pointer_t = decltype(function_pointer(std::declval<Type>()...));
 
 template<typename... Class, typename Ret, typename... Args>
 [[nodiscard]] constexpr auto index_sequence_for(Ret (*)(Args...)) {
