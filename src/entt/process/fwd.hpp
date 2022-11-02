24a9cd67ee1569e49219a24b462b4492d5df0cf7
Michele Caini <michele.caini@gmail.com>
2022-11-02 09:35:53 +0100

    scheduler: forgot to add the fwd file to the previous commit :)

--- src/entt/process/fwd.hpp
+++ src/entt/process/fwd.hpp
@@ -1,13 +1,18 @@
 #ifndef ENTT_PROCESS_FWD_HPP
 #define ENTT_PROCESS_FWD_HPP
 
+#include <cstdint>
+
 namespace entt {
 
 template<typename, typename>
 class process;
 
-template<typename>
-class scheduler;
+template<typename = std::uint32_t>
+class basic_scheduler;
+
+/*! @brief Alias declaration for the most common use case. */
+using scheduler = basic_scheduler<>;
 
 } // namespace entt
 
