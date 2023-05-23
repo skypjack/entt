85bffb71435dfa126e6ffda77630ed2dbd67910d
Dominic Koepke <DNKpp2011@gmail.com>
2023-05-23 13:56:29 +0200

    type_traits: std::tuple traits specialization for entt::type_list and entt::value_list (#1011)

--- src/entt/core/type_traits.hpp
+++ src/entt/core/type_traits.hpp
@@ -3,6 +3,7 @@
 
 #include <cstddef>
 #include <iterator>
+#include <tuple>
 #include <type_traits>
 #include <utility>
 #include "../config/config.h"
@@ -387,7 +388,18 @@ template<auto Value, auto... Other>
 struct value_list_element<0u, value_list<Value, Other...>> {
     /*! @brief Searched value. */
     static constexpr auto value = Value;
+
+    /*! @brief Searched type. */
+    using type = decltype(Value);
 };
+
+/**
+ * @brief Helper type.
+ * @tparam Index Index of the type to return.
+ * @tparam List Value list to search into.
+ */
+template<std::size_t Index, typename List>
+using value_list_element_t = typename value_list_element<Index, List>::type;
 
 /**
  * @brief Helper type.
@@ -893,5 +905,17 @@ template<std::size_t Index, auto Candidate>
 using nth_argument_t = typename nth_argument<Index, Candidate>::type;
 
 } // namespace entt
+
+template <typename... Type>
+struct std::tuple_size<entt::type_list<Type...>>: std::integral_constant<std::size_t, entt::type_list<Type...>::size> {};
+
+template <std::size_t Index, typename... Type>
+struct std::tuple_element<Index, entt::type_list<Type...>>: entt::type_list_element<Index, entt::type_list<Type...>> {};
+
+template <auto... Value>
+struct std::tuple_size<entt::value_list<Value...>>: std::integral_constant<std::size_t, entt::value_list<Value...>::size> {};
+
+template <std::size_t Index, auto... Value>
+struct std::tuple_element<Index, entt::value_list<Value...>>: entt::value_list_element<Index, entt::value_list<Value...>> {};
 
 #endif
