d15fd2a8b7e48ae5565c863512ea5a4cee4aefc2
Michele Caini <michele.caini@gmail.com>
2023-09-18 14:07:14 +0200

    test: ENTT_DEBUG_TEST_P

--- test/entt/common/config.h
+++ test/entt/common/config.h
@@ -5,10 +5,12 @@ namespace test {
 
 #ifdef NDEBUG
 #    define ENTT_DEBUG_TEST(Case, Test) TEST(Case, DISABLED_##Test)
+#    define ENTT_DEBUG_TEST_P(Case, Test) TEST_P(Case, DISABLED_##Test)
 #    define ENTT_DEBUG_TEST_F(Case, Test) TEST_F(Case, DISABLED_##Test)
 #    define ENTT_DEBUG_TYPED_TEST(Case, Test) TYPED_TEST(Case, DISABLED_##Test)
 #else
 #    define ENTT_DEBUG_TEST(Case, Test) TEST(Case, Test)
+#    define ENTT_DEBUG_TEST_P(Case, Test) TEST_P(Case, Test)
 #    define ENTT_DEBUG_TEST_F(Case, Test) TEST_F(Case, Test)
 #    define ENTT_DEBUG_TYPED_TEST(Case, Test) TYPED_TEST(Case, Test)
 #endif
