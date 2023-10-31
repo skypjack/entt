ad99c4eb140480fc6adb22f04dd3f8814c70d47b
Michele Caini <michele.caini@gmail.com>
2023-10-31 09:18:13 +0100

    test: shared non default constructible type

---
+++ test/entt/common/non_default_constructible.h
@@ -0,0 +1,19 @@
+#ifndef ENTT_COMMON_NON_DEFAULT_CONSTRUCTIBLE_HPP
+#define ENTT_COMMON_NON_DEFAULT_CONSTRUCTIBLE_HPP
+
+#include <type_traits>
+
+namespace test {
+
+struct non_default_constructible {
+    non_default_constructible() = delete;
+
+    non_default_constructible(int v)
+        : value{v} {}
+
+    int value;
+};
+
+} // namespace test
+
+#endif
