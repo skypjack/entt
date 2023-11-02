76516e0320566f80fcb431eed7b358d7e1c57ccb
Michele Caini <michele.caini@gmail.com>
2023-11-02 15:05:16 +0100

    test: cleanup

--- test/entt/common/pointer_stable.h
+++ test/entt/common/pointer_stable.h
@@ -1,8 +1,6 @@
 #ifndef ENTT_COMMON_POINTER_STABLE_HPP
 #define ENTT_COMMON_POINTER_STABLE_HPP
 
-#include <type_traits>
-
 namespace test {
 
 struct pointer_stable {
@@ -17,9 +15,6 @@ inline bool operator==(const pointer_stable &lhs, const pointer_stable &rhs) {
 inline bool operator<(const pointer_stable &lhs, const pointer_stable &rhs) {
     return lhs.value < rhs.value;
 }
-
-// ensure that we've at least an aggregate type to test here
-static_assert(std::is_aggregate_v<test::pointer_stable>, "Not an aggregate type");
 
 } // namespace test
 
