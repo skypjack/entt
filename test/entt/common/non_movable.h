6e1a5c03531c28c9e532e2f4591d18002852ef9a
Michele Caini <michele.caini@gmail.com>
2023-11-03 12:48:40 +0100

    test: shared non_movable type

---
+++ test/entt/common/non_movable.h
@@ -0,0 +1,20 @@
+#ifndef ENTT_COMMON_NON_MOVABLE_HPP
+#define ENTT_COMMON_NON_MOVABLE_HPP
+
+namespace test {
+
+struct non_movable {
+    non_movable() = default;
+
+    non_movable(const non_movable &) = default;
+    non_movable(non_movable &&) = delete;
+
+    non_movable &operator=(const non_movable &) = default;
+    non_movable &operator=(non_movable &&) = delete;
+
+    int value{};
+};
+
+} // namespace test
+
+#endif
