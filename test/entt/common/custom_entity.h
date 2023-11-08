9b4c920d1fa10690a32afb48b44b20136ed12a7e
Michele Caini <michele.caini@gmail.com>
2023-11-08 12:37:54 +0100

    test: shared custom_entity type

---
+++ test/entt/common/custom_entity.h
@@ -0,0 +1,12 @@
+#ifndef ENTT_COMMON_CUSTOM_ENTITY_HPP
+#define ENTT_COMMON_CUSTOM_ENTITY_HPP
+
+#include <cstdint>
+
+namespace test {
+
+enum custom_entity : std::uint32_t {};
+
+} // namespace test
+
+#endif
