1ff2db8aa290cad41e48e0b94a494bbf1652f8ca
Michele Caini <michele.caini@gmail.com>
2024-01-08 14:25:03 +0100

    test: drop unnecessary checks

--- test/entt/entity/runtime_view.cpp
+++ test/entt/entity/runtime_view.cpp
@@ -90,7 +90,6 @@ TYPED_TEST(RuntimeView, Constructors) {
     const runtime_view_type other{std::move(temp), view.get_allocator()};
 
     ASSERT_TRUE(view.contains(entity));
-    ASSERT_FALSE(temp.contains(entity)); // NOLINT
     ASSERT_TRUE(other.contains(entity));
 }
 
@@ -140,7 +139,6 @@ TYPED_TEST(RuntimeView, Move) {
 
     runtime_view_type other{std::move(view)};
 
-    ASSERT_FALSE(view.contains(entity)); // NOLINT
     ASSERT_TRUE(other.contains(entity));
 
     view = other;
@@ -151,7 +149,6 @@ TYPED_TEST(RuntimeView, Move) {
 
     other = std::move(view);
 
-    ASSERT_FALSE(view.contains(entity)); // NOLINT
     ASSERT_TRUE(other.contains(entity));
 }
 
