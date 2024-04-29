bf854b0b5fc31f3fb42b69d3766c72c77f818a6c
Michele Caini <michele.caini@gmail.com>
2024-04-29 09:01:54 +0200

    table: let's write some tests

--- test/entt/entity/table.cpp
+++ test/entt/entity/table.cpp
@@ -1,6 +1,20 @@
+#include <memory>
+#include <tuple>
+#include <type_traits>
+#include <utility>
 #include <gtest/gtest.h>
+#include <entt/core/iterator.hpp>
 #include <entt/entity/table.hpp>
+#include "../../common/linter.hpp"
 
-TEST(Table, Placeholder) {
-    [[maybe_unused]] entt::table<int, char> table{};
+TEST(Table, Constructors) {
+    entt::table<int, char> table;
+
+    ASSERT_NO_THROW([[maybe_unused]] auto alloc = table.get_allocator());
+
+    table = entt::table<int, char>{std::allocator<void>{}};
+
+    ASSERT_NO_THROW([[maybe_unused]] auto alloc = table.get_allocator());
+}
+
 }
