9edeb731f5e622176715516bcd0f3434c5829fb3
Michele Caini <michele.caini@gmail.com>
2024-05-10 15:25:20 +0200

    test: ENTT_NO_MIXIN

--- test/entt/entity/storage_utility.cpp
+++ test/entt/entity/storage_utility.cpp
@@ -14,16 +14,28 @@ TYPED_TEST(StorageUtility, StorageType) {
     using value_type = typename TestFixture::type;
 
     // just a bunch of static asserts to avoid regressions
+#ifdef ENTT_NO_MIXIN
+    testing::StaticAssertTypeEq<entt::storage_type_t<value_type, entt::entity>, entt::basic_storage<value_type, entt::entity>>();
+    testing::StaticAssertTypeEq<entt::storage_type_t<value_type>, entt::storage<value_type>>();
+#else
     testing::StaticAssertTypeEq<entt::storage_type_t<value_type, entt::entity>, entt::sigh_mixin<entt::basic_storage<value_type, entt::entity>>>();
     testing::StaticAssertTypeEq<entt::storage_type_t<value_type>, entt::sigh_mixin<entt::storage<value_type>>>();
+#endif // ENTT_NO_MIXIN
 }
 
 TYPED_TEST(StorageUtility, StorageFor) {
     using value_type = typename TestFixture::type;
 
     // just a bunch of static asserts to avoid regressions
+#ifdef ENTT_NO_MIXIN
+    testing::StaticAssertTypeEq<entt::storage_for_t<const value_type, entt::entity>, const entt::basic_storage<value_type, entt::entity>>();
+    testing::StaticAssertTypeEq<entt::storage_for_t<value_type, entt::entity>, entt::basic_storage<value_type, entt::entity>>();
+    testing::StaticAssertTypeEq<entt::storage_for_t<const value_type>, const entt::storage<value_type>>();
+    testing::StaticAssertTypeEq<entt::storage_for_t<value_type>, entt::storage<value_type>>();
+#else
     testing::StaticAssertTypeEq<entt::storage_for_t<const value_type, entt::entity>, const entt::sigh_mixin<entt::basic_storage<value_type, entt::entity>>>();
     testing::StaticAssertTypeEq<entt::storage_for_t<value_type, entt::entity>, entt::sigh_mixin<entt::basic_storage<value_type, entt::entity>>>();
     testing::StaticAssertTypeEq<entt::storage_for_t<const value_type>, const entt::sigh_mixin<entt::storage<value_type>>>();
     testing::StaticAssertTypeEq<entt::storage_for_t<value_type>, entt::sigh_mixin<entt::storage<value_type>>>();
+#endif
 }
