e5804ac0a2ecb277a5de5fa8a16c82d376baa6cf
Michele Caini <michele.caini@gmail.com>
2024-05-20 13:20:48 +0200

    *: use transparent comparators

--- src/entt/container/fwd.hpp
+++ src/entt/container/fwd.hpp
@@ -12,14 +12,14 @@ template<
     typename Key,
     typename Type,
     typename = std::hash<Key>,
-    typename = std::equal_to<Key>,
+    typename = std::equal_to<>,
     typename = std::allocator<std::pair<const Key, Type>>>
 class dense_map;
 
 template<
     typename Type,
     typename = std::hash<Type>,
-    typename = std::equal_to<Type>,
+    typename = std::equal_to<>,
     typename = std::allocator<Type>>
 class dense_set;
 
