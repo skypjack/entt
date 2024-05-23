9db1d085f98d7eae4ad0a6ffec6b6fbfbbdde611
Michele Caini <michele.caini@gmail.com>
2024-05-23 08:22:18 +0200

    macro: suppress macro usage linter messages

--- src/entt/config/macro.h
+++ src/entt/config/macro.h
@@ -1,7 +1,11 @@
 #ifndef ENTT_CONFIG_MACRO_H
 #define ENTT_CONFIG_MACRO_H
 
+// NOLINTBEGIN(cppcoreguidelines-macro-usage)
+
 #define ENTT_STR(arg) #arg
 #define ENTT_XSTR(arg) ENTT_STR(arg)
+
+// NOLINTEND(cppcoreguidelines-macro-usage)
 
 #endif
