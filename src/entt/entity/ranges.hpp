9e7985363ee39bf6dc074767f0ac4a71cb9f4678
Michele Caini <michele.caini@gmail.com>
2024-07-09 10:11:47 +0200

    entity: update guard for ranges

--- src/entt/entity/ranges.hpp
+++ src/entt/entity/ranges.hpp
@@ -1,10 +1,12 @@
 #ifndef ENTT_ENTITY_RANGES_HPP
 #define ENTT_ENTITY_RANGES_HPP
 
-#if __cplusplus >= 202002L
-
-#    include <ranges>
-#    include "fwd.hpp"
+#if __has_include(<version>)
+#    include <version>
+#
+#    if defined(__cpp_lib_ranges)
+#        include <ranges>
+#        include "fwd.hpp"
 
 template<class... Args>
 inline constexpr bool std::ranges::enable_borrowed_range<entt::basic_view<Args...>>{true};
@@ -18,6 +20,7 @@ inline constexpr bool std::ranges::enable_view<entt::basic_view<Args...>>{true};
 template<class... Args>
 inline constexpr bool std::ranges::enable_view<entt::basic_group<Args...>>{true};
 
+#    endif
 #endif
 
 #endif
