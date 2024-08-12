53b0efa87d4e77a18ca41123655f71d4359e724c
Tozic <70832788+xNotTozic@users.noreply.github.com>
2024-08-12 15:43:06 +0400

    Enhance component_traits with advanced type traits and concepts

--- src/entt/entity/component.hpp
+++ src/entt/entity/component.hpp
@@ -8,46 +8,61 @@
 
 namespace entt {
 
-/*! @cond TURN_OFF_DOXYGEN */
 namespace internal {
 
-template<typename Type, typename = void>
-struct in_place_delete: std::bool_constant<!(std::is_move_constructible_v<Type> && std::is_move_assignable_v<Type>)> {};
+    // Concept to check for the presence of a static member `in_place_delete`
+    template<typename Type, typename = void>
+    concept HasInPlaceDelete = requires {
+        { Type::in_place_delete } -> std::convertible_to<bool>;
+    };
 
-template<>
-struct in_place_delete<void>: std::false_type {};
+    // Concept to check for the presence of a static member `page_size`
+    template<typename Type, typename = void>
+    concept HasPageSize = requires {
+        { Type::page_size } -> std::convertible_to<std::size_t>;
+    };
 
-template<typename Type>
-struct in_place_delete<Type, std::enable_if_t<Type::in_place_delete>>
-    : std::true_type {};
+    // In-place delete trait with advanced type checks
+    template<typename Type, typename = void>
+    struct in_place_delete
+        : std::bool_constant<!std::is_move_constructible_v<Type> || !std::is_move_assignable_v<Type>> {};
 
-template<typename Type, typename = void>
-struct page_size: std::integral_constant<std::size_t, !std::is_empty_v<ENTT_ETO_TYPE(Type)> * ENTT_PACKED_PAGE> {};
+    template<>
+    struct in_place_delete<void> : std::false_type {};
 
-template<>
-struct page_size<void>: std::integral_constant<std::size_t, 0u> {};
+    template<typename Type>
+    struct in_place_delete<Type, std::enable_if_t<HasInPlaceDelete<Type>>>
+        : std::integral_constant<bool, Type::in_place_delete> {};
 
-template<typename Type>
-struct page_size<Type, std::void_t<decltype(Type::page_size)>>
-    : std::integral_constant<std::size_t, Type::page_size> {};
+    // Page size trait with advanced checks and defaulting
+    template<typename Type, typename = void>
+    struct page_size
+        : std::integral_constant<std::size_t, (std::is_empty_v<ENTT_ETO_TYPE(Type)> ? ENTT_PACKED_PAGE : 0)> {};
+
+    template<>
+    struct page_size<void> : std::integral_constant<std::size_t, 0u> {};
+
+    template<typename Type>
+    struct page_size<Type, std::void_t<decltype(Type::page_size)>>
+        : std::integral_constant<std::size_t, Type::page_size> {};
 
 } // namespace internal
-/*! @endcond */
 
 /**
- * @brief Common way to access various properties of components.
- * @tparam Type Type of component.
+ * @brief Traits for accessing various properties of components with advanced type introspection.
+ * @tparam Type Component type.
  */
 template<typename Type, typename = void>
 struct component_traits {
-    static_assert(std::is_same_v<std::decay_t<Type>, Type>, "Unsupported type");
+    static_assert(std::is_same_v<std::decay_t<Type>, Type>, "Type must be non-reference and non-const/volatile.");
 
     /*! @brief Component type. */
     using type = Type;
 
-    /*! @brief Pointer stability, default is `false`. */
+    /*! @brief Indicates if the component supports in-place deletion, default is `false`. */
     static constexpr bool in_place_delete = internal::in_place_delete<Type>::value;
-    /*! @brief Page size, default is `ENTT_PACKED_PAGE` for non-empty types. */
+
+    /*! @brief Size of the page used for storing components. Defaults to `ENTT_PACKED_PAGE` if unspecified. */
     static constexpr std::size_t page_size = internal::page_size<Type>::value;
 };
 
