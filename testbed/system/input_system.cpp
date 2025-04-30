57a6c6b23873dc6da1e908060f5fd46874f85365
skypjack <michele.caini@gmail.com>
2025-04-30 16:06:55 +0200

    testbed: bare minimal input system

--- testbed/system/input_system.cpp
+++ testbed/system/input_system.cpp
@@ -1,8 +1,19 @@
 #include <application/context.h>
+#include <component/input_listener_component.h>
 #include <entt/entity/registry.hpp>
 #include <system/input_system.h>
 
 namespace testbed {
+
+namespace internal {
+
+static void update_listeners(entt::registry &registry, input_listener_component::type command) {
+    for([[maybe_unused]] auto [entt, elem]: registry.view<input_listener_component>().each()) {
+        elem.command = command;
+    }
+}
+
+} // namespace internal
 
 void input_system(entt::registry &registry, const SDL_Event &event, bool &quit) {
     switch(event.type) {
@@ -13,6 +24,18 @@ void input_system(entt::registry &registry, const SDL_Event &event, bool &quit) 
         switch(event.key.key) {
         case SDLK_ESCAPE:
             quit = true;
+            break;
+        case SDLK_UP:
+            internal::update_listeners(registry, input_listener_component::type::UP);
+            break;
+        case SDLK_DOWN:
+            internal::update_listeners(registry, input_listener_component::type::DOWN);
+            break;
+        case SDLK_LEFT:
+            internal::update_listeners(registry, input_listener_component::type::LEFT);
+            break;
+        case SDLK_RIGHT:
+            internal::update_listeners(registry, input_listener_component::type::RIGHT);
             break;
         }
         break;
