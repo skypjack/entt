340888e4e222027f67f160bb28e55a4114162492
skypjack <michele.caini@gmail.com>
2025-04-30 16:24:01 +0200

    testbed: bare minimum rendering system

--- testbed/system/rendering_system.cpp
+++ testbed/system/rendering_system.cpp
@@ -1,13 +1,28 @@
+#include <SDL3/SDL_render.h>
 #include <application/context.h>
+#include <component/position_component.h>
+#include <component/rect_component.h>
+#include <component/renderable_component.h>
 #include <entt/entity/registry.hpp>
 #include <system/rendering_system.h>
 
 namespace testbed {
 
 void rendering_system(entt::registry &registry, const context &ctx) {
-    // render...
-    static_cast<void>(registry);
-    static_cast<void>(ctx);
+    constexpr int logical_width = 1920;
+    constexpr int logical_height = 1080;
+
+    SDL_SetRenderLogicalPresentation(ctx, logical_width, logical_height, SDL_LOGICAL_PRESENTATION_LETTERBOX);
+    SDL_SetRenderDrawColor(ctx, 0u, 0u, 0u, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(ctx);
+
+    for(auto [entt, pos, rect]: registry.view<renderable_component, position_component, rect_component>().each()) {
+        SDL_FRect elem{rect.area.x + pos.point.x, rect.area.y + pos.point.y, rect.area.w, rect.area.h};
+        SDL_SetRenderDrawColor(ctx, 255u, 255u, 255u, SDL_ALPHA_OPAQUE);
+        SDL_RenderRect(ctx, &elem);
+    }
+
+    SDL_SetRenderLogicalPresentation(ctx, 0, 0, SDL_LOGICAL_PRESENTATION_DISABLED);
 }
 
 } // namespace testbed
