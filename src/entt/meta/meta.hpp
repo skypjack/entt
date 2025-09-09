7d2b4fcf57f6a7161d840396fc38973c57ea9983
skypjack <michele.caini@gmail.com>
2025-09-09 17:30:09 +0200

    meta: internal changes

--- src/entt/meta/meta.hpp
+++ src/entt/meta/meta.hpp
@@ -518,9 +518,12 @@ public:
     template<typename Type, typename... Args>
     void emplace(Args &&...args) {
         storage.emplace<Type>(std::forward<Args>(args)...);
-        resolve = internal::resolve<std::remove_cv_t<std::remove_reference_t<Type>>>;
-        lazy_node = internal::meta_type_node{};
-        vtable = &basic_vtable<std::remove_cv_t<std::remove_reference_t<Type>>>;
+
+        if(auto *overload = internal::resolve<std::remove_cv_t<std::remove_reference_t<Type>>>; overload != resolve) {
+            resolve = overload;
+            lazy_node = internal::meta_type_node{};
+            vtable = &basic_vtable<std::remove_cv_t<std::remove_reference_t<Type>>>;
+        }
     }
 
     /*! @copydoc any::assign */
