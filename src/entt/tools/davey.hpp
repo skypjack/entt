84fee2848f64542f8813beb5b2f5772c856beee5
skypjack <michele.caini@gmail.com>
2025-09-24 15:42:37 +0200

    tools: update davey

--- src/entt/tools/davey.hpp
+++ src/entt/tools/davey.hpp
@@ -42,7 +42,7 @@ static void present_element(const meta_any &obj, OnEntity on_entity) {
             const char *as_string = nullptr;
 
             for(auto [id, curr]: type.data()) {
-                if(curr.get(nullptr) == elem) {
+                if(curr.get(entt::meta_any{}) == elem) {
                     as_string = curr.name();
                     break;
                 }
