889d6217d1e24c6b5b714a8c221b31f222ec87c0
skypjack <michele.caini@gmail.com>
2025-11-21 16:55:45 +0100

    now working on version 4.0.x

--- src/entt/config/version.h
+++ src/entt/config/version.h
@@ -5,8 +5,8 @@
 
 // NOLINTBEGIN(cppcoreguidelines-macro-*,modernize-macro-*)
 
-#define ENTT_VERSION_MAJOR 3
-#define ENTT_VERSION_MINOR 16
+#define ENTT_VERSION_MAJOR 4
+#define ENTT_VERSION_MINOR 0
 #define ENTT_VERSION_PATCH 0
 
 #define ENTT_VERSION \
