ed590b7ac89dcc4dced9c81e64bbd10985d148ec
skypjack <michele.caini@gmail.com>
2026-02-06 18:33:57 +0100

    concepts: allocator_like

--- src/entt/core/concepts.hpp
+++ src/entt/core/concepts.hpp
@@ -1,6 +1,7 @@
 #ifndef ENTT_CORE_CONCEPTS_HPP
 #define ENTT_CORE_CONCEPTS_HPP
 
+#include <concepts>
 #include <type_traits>
 
 namespace entt {
@@ -11,6 +12,16 @@ namespace entt {
  */
 template<typename Type>
 concept cvref_unqualified = std::is_same_v<std::remove_cvref_t<Type>, Type>;
+
+/**
+ * @brief Specifies that a type is likely an allocator type.
+ * @tparam Type Type to check.
+ */
+template<typename Type>
+concept allocator_like = requires(Type alloc, typename Type::value_type *value) {
+    { alloc.allocate(0) } -> std::same_as<decltype(value)>;
+    { alloc.deallocate(value, 0) } -> std::same_as<void>;
+};
 
 } // namespace entt
 
