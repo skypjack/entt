a5f5fdb00116730a48536c4f404c247ad3ba1beb
skypjack <michele.caini@gmail.com>
2026-07-29 17:56:34 +0200

    dense_set: linter stuff

--- src/entt/container/dense_set.hpp
+++ src/entt/container/dense_set.hpp
@@ -418,6 +418,7 @@ public:
      * internal array.
      */
     [[nodiscard]] const_iterator cend() const noexcept {
+        // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic) - waiting for C++20 (and stl::span)
         return packed.first().data() + packed.first().size();
     }
 
@@ -428,6 +429,7 @@ public:
 
     /*! @copydoc end */
     [[nodiscard]] iterator end() noexcept {
+        // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic) - waiting for C++20 (and stl::span)
         return packed.first().data() + packed.first().size();
     }
 
